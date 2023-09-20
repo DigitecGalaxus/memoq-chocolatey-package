d66457e2a2a4c35453ce2482b1b921c2d3d0e8b9
Christian Margadant <christian.margadant@digitecgalaxus.ch>
2023-09-20 09:20:44 +0200

    Reworked installation

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,29 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$version = "10.3.10"
+$version = "10.2.11"
 $url = "https://dl.memoq.com/memoq/memoQ-$version.exe"
-$checksum = "E6E92247F9F0152BDFCC7FC61216622364E5D20DCF285384E31D7E30F9F60396"
+$checksum = "C7E893EEF0A6F2BC4D87D6932A46DCC1EBC8A0FB17D5988DD1ED67CFCC2A26CA"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  fileType      = 'EXE'
   url           = $url
   file          = "$toolsDir\memoQ-$version.exe"
-
+  fileType      = 'EXE'
   checksum      = $checksum
   checksumType  = 'sha256'
-
   silentArgs    = "/VERYSILENT /CLOSEAPPLICATIONS /NORESTART /LOG `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).EXEInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyPackage `
-  -PackageName $packageArgs.packageName `
-  -Url $packageArgs.url `
-  -File $packageArgs.file `
-  -FileType $packageArgs.fileType `
-  -Checksum $packageArgs.checksum `
-  -ChecksumType $packageArgs.checksumType `
-  -SilentArgs $packageArgs.silentArgs `
-  -ValidExitCodes $packageArgs.validExitCodes
+Install-ChocolateyPackage @packageArgs
