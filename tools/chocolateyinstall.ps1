e2408e892e06fb3649cb79dd62ee7025296700e4
ChrisMargadant <140088150+ChrisMargadant@users.noreply.github.com>
2023-11-07 13:43:03 +0100

    10.3.10 review (#2)
    * Reworked installation
    
    * Fixed issue in memoq.nuspec
    
    * Updated to v10.3.10
    
    * Updated to v10.3.12

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,29 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$version = "10.3.10"
+$version = "10.3.12"
 $url = "https://dl.memoq.com/memoq/memoQ-$version.exe"
-$checksum = "E6E92247F9F0152BDFCC7FC61216622364E5D20DCF285384E31D7E30F9F60396"
+$checksum = "12E2E35BCAA96F11E6B70348DBA26C762977C71DDFBA346E37B9C22443067822"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  fileType      = 'EXE'
   url           = $url
   file          = "$toolsDir\memoQ-$version.exe"
-
+  fileType      = 'EXE'
   checksum      = $checksum
   checksumType  = 'sha256'
-
   silentArgs    = "/VERYSILENT /CLOSEAPPLICATIONS /NORESTART /LOG `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).EXEInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyPackage `
-  -PackageName $packageArgs.packageName `
-  -Url $packageArgs.url `
-  -File $packageArgs.file `
-  -FileType $packageArgs.fileType `
-  -Checksum $packageArgs.checksum `
-  -ChecksumType $packageArgs.checksumType `
-  -SilentArgs $packageArgs.silentArgs `
-  -ValidExitCodes $packageArgs.validExitCodes
+Install-ChocolateyPackage @packageArgs
