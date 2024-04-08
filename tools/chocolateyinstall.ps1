b0453162e5b7faa70d7b936487927abfc2ffabca
Christian Margadant <christian.margadant@digitecgalaxus.ch>
2024-04-08 17:22:29 +0200

    Update to v10.6.8

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$version = "10.4.12"
+$version = "10.6.8"
 $url = "https://dl.memoq.com/memoq/memoQ-$version.exe"
-$checksum = "CA1387B2085381BB9FA4A1EEBA72A702698F15CAE0B729F1087A183FFEF9DC50"
+$checksum = "37100C481F1991BE9CF25616CDD3B45B6DF9E9F675A9041409FEFE31C5D54649"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
